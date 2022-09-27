b1364870c014791b018f2d2a3d5b914082472d20
stephen ligtenberg <bountyhunter24@gmail.com>
2022-09-27 15:43:11 -0400

    commit tree 56bbe1485583b1f855933fd6cdf61ed1e2cd2f75

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(arrayStrings, event) {
+    const newArrayStrings = [];
+    for (let i = 0; i < arrayStrings.length; i++) {
+        newArrayStrings[i] = `Thank you, ${arrayStrings[i]}, for the wonderful ${event} gift!`;
+    }
+    return newArrayStrings;
+}
+
+function countDown(startNum) {
+    for (let i = startNum; i >= 0; i--) {
+        console.log(i);
+    }
+}
